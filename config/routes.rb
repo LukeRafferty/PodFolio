3a384a6f8f084b70b2f378b1b62516f1bf40c8fd
Luke Rafferty <luke.mingins.rafferty@gmail.com>
2017-08-03 13:21:59 -0400

    sanity commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # These routes will be for signup. The first renders a form in the browse, the second will
   # receive the form and create a user in our database using the data given to us by the user.
 
-
+  get '/home' => "podcasts#index"
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
